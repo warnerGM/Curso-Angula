0bc4adb54593bd99879183665357e8482147bbc5
warner <w.gomez@sisalril.gob.do>
2023-06-05 15:59:01 -0400

    seccion17 episodio 234

---
+++ 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup ,Validators} from '@angular/forms';
+
+
+
+@Component({
+  templateUrl: './basic-page.component.html',
+  styles: [
+  ]
+})
+export class BasicPageComponent implements OnInit {
+
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0)
+    
+  // })
+
+  public myForm:FormGroup= this.fb.group({
+    name:['',[Validators.required ,Validators.minLength(3) ]],
+    price: [0,[Validators.required ,Validators.min(3) ]],
+    instorage: [0,[Validators.required, Validators.min(0)]],
+  })
+
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(): void {
+    this.myForm.reset({
+
+    })
+  }
+
+  onSave():void{
+    if(this.myForm.invalid)return;
+
+    console.log(this.myForm.value);
+    this.myForm.reset({price:0, instorage:0});
+  }
+}
