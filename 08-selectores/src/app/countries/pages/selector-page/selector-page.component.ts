6d847ffab2418a9f0e6fa0426bd42cf84f3692ef
warner <w.gomez@sisalril.gob.do>
2023-06-09 15:58:03 -0400

    seccion 19 episodio 264

---
+++ 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interface';
+import { switchMap } from 'rxjs';
+
+@Component({
+  selector: 'selector-page',
+  templateUrl: './selector-page.component.html',
+  styles: [
+  ]
+})
+export class SelectorPageComponent implements OnInit {
+
+  public myForm:FormGroup =this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    borders: ['', Validators.required],
+  })
+
+
+  constructor(
+    private fb:FormBuilder,
+    private countriesService:CountriesService
+  ){}
+
+  ngOnInit(): void {
+   this.onRegionChanged();
+  }
+
+  
+
+
+   get regions(): Region[]{
+    return this.countriesService.regions;
+   }
+
+   onRegionChanged(): void{
+    this.myForm.get('region')!.valueChanges
+    .pipe(
+      switchMap(region => this.countriesService.getCountriesByRegion(region))
+    )
+    .subscribe(coutries =>{
+      console.log({coutries})
+    });
+
+   }
+
+}
---
+++ 08-selectores/src/app/countries/services/countries.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Region, SmallCountry } from '../interfaces/country.interface';
+import { Observable, of, tap } from 'rxjs';
+import {HttpClient} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountriesService {
+
+  private baseUrl:string='https://restcountries.com/v3.1';
+
+  private _regions: Region[]=[Region.Africa, Region.Americas,Region.Asia,Region.Europe,Region.Oceanian];
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  get regions(): Region[]{
+    return [...this._regions];
+  }
+
+  getCountriesByRegion(region:Region):Observable<SmallCountry[]>{
+
+    if(!region) return of([]);
+
+    const url: string = `${this.baseUrl}/region/${region}?field=cca3,name,borders`
+
+    
+    return this.http.get<SmallCountry[]>(url)
+    .pipe(
+      tap(response => console.log({response}))
+    )
+  }
+
+}
